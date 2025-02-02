d58cbf4daac647d478e3b74bce8a122ad3451789
dminno <demeter.marcell@gmail.com>
2025-02-02 00:05:43 +0000

    Adding basic structure and toggle screen function v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,55 @@
-console.log("Hello World!");
+const launchBtn = document.getElementById("launch");
+const endGameBtn = document.getElementById("end-game");
+const restartBtn = document.getElementById("restart");
+
+launchBtn.addEventListener("click", gameStart);
+endGameBtn.addEventListener("click", gameEnd);
+restartBtn.addEventListener("click", gameRestart);
+
+// function gameStart (e){
+//     toggleScreen();
+// }
+
+// function gameEnd (e){
+//     toggleScreen();
+// }
+
+// function gameEnd (e){
+//     toggleScreen();
+// }
+
+
+// function toggleScreen(){
+//     const currentScreen = document.getElementsByClassName.contains
+// }
+
+
+function gameStart (e){
+    const prevSections = document.getElementsByClassName("screen1");
+    const nextSections = document.getElementsByClassName("screen2");
+    for (let section of prevSections){
+        section.classList.toggle("hide");}
+    
+        for (let element of nextSections){
+        element.classList.toggle("hide");}
+}
+
+function gameEnd (e){
+    const prevSections = document.getElementsByClassName("screen2");
+    const nextSections = document.getElementsByClassName("screen3");
+    for (let section of prevSections){
+        section.classList.toggle("hide");}
+    
+        for (let element of nextSections){
+        element.classList.toggle("hide");}
+}
+
+function gameRestart (e){
+    const prevSections = document.getElementsByClassName("screen3");
+    const nextSections = document.getElementsByClassName("screen1");
+    for (let section of prevSections){
+        section.classList.toggle("hide");}
+    
+        for (let element of nextSections){
+        element.classList.toggle("hide");}
+}
